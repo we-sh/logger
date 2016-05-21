9e019b36e9903481fb0f4069823961a75a55ca3a
Adrien Nouvel <adrien.nouvel@outlook.com>
2016-05-21 14:49:41 +0200

    because adding \n on the ouput, it is not necessary to put it in the message

--- test/main.c
+++ test/main.c
@@ -16,12 +16,12 @@ int	main(void)
 {
 	if (logger_init(D_TRACE, "out.log") != 0)
 		printf("failed to open the logger\n");
-	log_fatal("%s\n", "lol");
-	log_error("%s\n", "lol");
-	log_warn("%s\n", "lol");
-	log_info("%s\n", "lol");
-	log_debug("%s\n", "lol");
-	log_trace("%s\n", "lol");
+	log_fatal("%s", "lol");
+	log_error("%s", "lol");
+	log_warn("%s", "lol");
+	log_info("%s", "lol");
+	log_debug("%s", "lol");
+	log_trace("%s", "lol");
 	logger_close();
 	return (0);
 }
